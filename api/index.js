217ac0b7b5396fb4bdc81a673dbd7928516b14c9
Gourav Pal <gouravsir99@gmail.com>
2026-08-19 07:36:32 +0530

    fix backend connection

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-const app = require("../backend/app");
-const connectToMongo = require("../backend/db");
+const app = require("../Backend/app");
+const connectToMongo = require("../Backend/db");
 
 module.exports = async (req, res) => {
   try {
     await connectToMongo();
-
     return app(req, res);
   } catch (error) {
-    console.error("Database error:", error);
+    console.error("SERVER ERROR:", error);
 
     return res.status(500).json({
-      error: "Database connection failed",
+      success: false,
+      error: error.message,
     });
   }
 };
